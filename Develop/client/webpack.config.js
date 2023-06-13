14f610d28a98766d07d35a64dd938caf8400131c
Brandon Cullifer <brandon.cullifer@gmail.com>
2023-06-13 15:43:30 -0400

    first commit

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,12 +18,45 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'cautious meme',
+        short_name: 'CM',
+        description: 'My meme',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+        icons: [
+          {
+            src: path.resolve('./client/src/favicon.ico'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('icon', )  // multiple sizes
+          },
+
+        ]
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
